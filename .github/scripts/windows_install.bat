520219771c0e82c3f85d271a21deefe6d6d44de1
jelletreep <40466121+jelletreep@users.noreply.github.com>
2019-10-22 12:55:13 +0200

    change call conda environments for batch files

--- .github/scripts/windows_install.bat
+++ .github/scripts/windows_install.bat
@@ -1,4 +1,4 @@
 SET PATH=%PYTHON%;%PYTHON%\\Scripts;%PATH%
 conda env create -f environment_py%PY_VERSION%_%OS_NAME%.yml -n parcels
-CALL conda.bat activate parcels
+call activate parcels
 python setup.py install
