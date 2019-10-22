90889cab49556e2b7d295fabcb42b9751cc49e58
jelletreep <40466121+jelletreep@users.noreply.github.com>
2019-10-22 16:15:40 +0200

    add python path in osx test script

--- .github/scripts/osx_test.sh
+++ .github/scripts/osx_test.sh
@@ -1,3 +1,5 @@
+export PATH="$HOME/miniconda/bin:$PATH"
+
 export CONDA_BUILD_SYSROOT=/
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:/Applications/Xcode.app/Contents//Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
 
